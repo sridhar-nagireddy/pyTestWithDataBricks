178616ba680f2d9c2bf27a9fcf51f924e72ade20
sridhar <sridharnagireddy@gmail.com>
2026-05-09 15:57:12 +1000

    removed main read

--- tests/test_with_chispa.py
+++ tests/test_with_chispa.py
@@ -4,11 +4,12 @@ import logging
 def test_data_integrity(spark):
  
     logger = logging.getLogger(__name__)
+    expected_data = [(1, "A"), (2, "B")]
     # Data from Databricks
-    actual_df = spark.read.table("main.default.my_table")
+    actual_df = spark.createDataFrame(expected_data, ["id", "value"])
     
     # Expected data you've defined locally
-    expected_data = [(1, "A"), (2, "B")]
+    
     expected_df = spark.createDataFrame(expected_data, ["id", "value"])
     
     # Chispa comparison
